c3c4f4eb53e81a8fedcf9653947ce5d39316f9fb
nongrubl89 <lisaburgnon@gmail.com>
2021-01-07 14:47:31 -0800

    redid the css

--- src/interface.js
+++ src/interface.js
@@ -1,223 +1,167 @@
-import { ListItem, Project, ProjectManager } from "./projectManager.js";
-import { DateTime } from "./dateTime.js";
+import { ListItem, Project, ProjectManager } from './projectManager.js';
+import { DateTime } from './dateTime.js';
 
 const renderInterface = (() => {
-  const container = document.querySelector(".project-container");
+  const container = document.querySelector('.content');
 
   //shows each project and renders the project div in the dom
   const renderProjects = (projects) => {
     projects.forEach(function (project, index, array) {
       if (index === 0) {
         project.projectID = 0;
-        // projectDiv.style.gridRowStart = rowCounter;
       }
       if (index === array.length - 1) {
-        const projectDiv = document.createElement("div");
-        projectDiv.className = "project-div";
+        const projectDiv = document.createElement('div');
+        projectDiv.className = 'slide project-div';
         projectDiv.dataset.id = index;
         project.projectID = index;
 
-        const titleDiv = document.createElement("div");
-        titleDiv.className = "title-div";
-        titleDiv.innerHTML = `<h4>${project.getTitle()}</h4>
-                <h5>Due: ${project.dateDue}</h5>`;
+        const titleDiv = document.createElement('div');
+        titleDiv.className = 'title-div';
+        titleDiv.innerHTML = `<h4>${project.getTitle()}</h4>`;
 
-        const taskList = document.createElement("ul");
+        const dateDiv = document.createElement('div');
+        dateDiv.className = 'date-div';
+        dateDiv.innerHTML = `<h4>${project.dateDue}</h4>`;
+
+        const taskList = document.createElement('div');
         taskList.dataset.number = index;
-        taskList.style.display = "none";
-        const buttonDiv = document.createElement("div");
-        buttonDiv.className = "button-div";
+        taskList.style.display = 'none';
 
         projectDiv.appendChild(titleDiv);
-        projectDiv.appendChild(buttonDiv);
+        projectDiv.appendChild(dateDiv);
         container.appendChild(projectDiv);
 
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "delete-project-button";
-        deleteButton.addEventListener("click", () => {
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'img-button delete-project-button';
+        deleteButton.addEventListener('click', () => {
           event.target.parentNode.parentNode.remove();
           ProjectManager.deleteProject(index);
         });
 
-        const addTaskButton = document.createElement("button");
-        addTaskButton.id = "add-task";
-        addTaskButton.className = "add-task-button";
-        addTaskButton.addEventListener("click", () => {
+        projectDiv.appendChild(deleteButton);
+
+        const addTaskButton = document.createElement('button');
+        addTaskButton.id = 'add-task';
+        addTaskButton.className = 'img-button add-task-button';
+        addTaskButton.addEventListener('click', () => {
           createNewTaskForm(project, projectDiv);
         });
 
-        const showTasksButton = document.createElement("button");
-        showTasksButton.className = "show-tasks";
-        showTasksButton.addEventListener("click", () => {
+        projectDiv.appendChild(addTaskButton);
+
+        const showTasksButton = document.createElement('button');
+        showTasksButton.className = 'img-button show-tasks';
+        showTasksButton.addEventListener('click', () => {
           if (
-            showTasksButton.className === "show-tasks" &&
+            showTasksButton.className === 'show-tasks' &&
             taskList.hasChildNodes() === false
           ) {
             console.log(taskList.hasChildNodes());
-            showTasksButton.className = "show-tasks";
+            showTasksButton.className = 'img-button show-tasks';
           } else {
-            showTasksButton.className = "hide-tasks";
+            showTasksButton.className = 'img-button hide-tasks';
           }
 
           if (project.toDos.length === 0) {
-            alert("No tasks in project");
+            alert('No tasks in project');
             return;
           }
 
-          if (taskList.style.display === "none") {
-            projectDiv.className = "project-div-expanded";
-            taskList.style.display = "grid";
+          if (taskList.style.display === 'none') {
+            projectDiv.className = 'project-div-expanded';
+            taskList.style.display = 'grid';
             shiftDivsDown();
 
-            if (projectDiv.className === "project-div") {
-              taskList.style.display = "none";
+            if (projectDiv.className === 'project-div') {
+              taskList.style.display = 'none';
             }
-          } else if (taskList.style.display === "grid") {
-            projectDiv.className = "project-div";
-            taskList.style.display = "none";
+          } else if (taskList.style.display === 'grid') {
+            projectDiv.className = 'project-div';
+            taskList.style.display = 'none';
             shiftDivsUp();
           }
         });
 
-        buttonDiv.appendChild(addTaskButton);
-        buttonDiv.appendChild(deleteButton);
-        buttonDiv.appendChild(showTasksButton);
+        projectDiv.appendChild(showTasksButton);
         projectDiv.appendChild(taskList);
-
-        addHover(
-          deleteButton,
-          createHoverDiv("deleteHover", "Delete this project", titleDiv)
-        );
-        addHover(
-          addTaskButton,
-          createHoverDiv("addTaskHover", "Add a new task", titleDiv)
-        );
-        addHover(
-          showTasksButton,
-          createHoverDiv("showTasksHover", "Show all tasks", titleDiv)
-        );
       }
     });
   };
 
-  //creates a hover div and appends it to an element
-  const createHoverDiv = (title, str, divToAppend) => {
-    title = document.createElement("div");
-    title.className = "hover-div";
-    title.innerHTML = str;
-    divToAppend.appendChild(title);
-    return title;
-  };
-
-  //adds a hover element to a div, used on the icons in each project div
-  const addHover = (ele, divToDisplay) => {
-    ele.onmouseover = function () {
-      divToDisplay.style.display = "block";
-    };
-    ele.onmouseout = function () {
-      divToDisplay.style.display = "none";
-    };
-  };
-
   //creates the text box and button for a new project
   const renderNewProjectButton = () => {
-    const sidebar = document.querySelector(".sidebar");
-    const newProjectFormDiv = document.createElement("div");
-    newProjectFormDiv.className = "col-sm-4";
-    newProjectFormDiv.id = "new-project-form-div";
+    const sidebar = document.querySelector('.sidebar');
+    const newProjectFormDiv = document.createElement('div');
+    newProjectFormDiv.innerHTML = `<h3 class ="header">Add a project</h3>`;
+    newProjectFormDiv.className = 'col-sm-4';
+    newProjectFormDiv.id = 'new-project-form-div';
     sidebar.appendChild(newProjectFormDiv);
 
     const newProjectInput = createDomInput(
-      "newProjectInput",
-      "form-group input-group form-control",
-      "Project Name"
+      'newProjectInput',
+      'form-group input-group form-control',
+      'Project Name'
     );
     newProjectFormDiv.appendChild(newProjectInput);
 
     newProjectFormDiv.appendChild(DateTime());
     $(function () {
-      $(".date").datetimepicker();
+      $('.date').datetimepicker();
     });
-    const calendar = document.querySelectorAll(".form-control")[1];
+    const calendar = document.querySelectorAll('.form-control')[1];
 
-    const newProjectButton = document.createElement("button");
-    newProjectButton.id = "new-project-button";
-    newProjectButton.innerHTML = "Create New Project";
+    const newProjectButton = document.createElement('button');
+    newProjectButton.id = 'new-project-button';
+    newProjectButton.innerHTML = 'Create New Project';
     newProjectFormDiv.appendChild(newProjectButton);
 
-    newProjectButton.addEventListener("click", () => {
+    newProjectButton.addEventListener('click', () => {
       if (!newProjectInput.value || !calendar.value) {
-        alert("Please fill in both fields");
+        alert('Please fill in both fields');
       } else {
-        // addColRow();
         let calendarValue = calendar.value;
         const newProjectObject = Project(
           newProjectInput.value,
-          calendarValue.substr(0, calendarValue.indexOf(" "))
+          calendarValue.substr(0, calendarValue.indexOf(' '))
         );
         ProjectManager.setProjects(newProjectObject);
         renderProjects(ProjectManager.projects);
-        calendarValue = "";
-        newProjectInput.value = "";
-      }
-    });
-  };
-
-  //shifts divs that follow the clicked div down when the expand button is clicked
-  const shiftDivsDown = () => {
-    const clickedDivID = event.target.parentNode.parentNode.dataset.id;
-    const projDivs = document.querySelectorAll("[data-id]");
-    const projDivsArray = Array.from(projDivs);
-    projDivsArray.forEach(function (div, i) {
-      if (i != clickedDivID) {
-        div.className = "project-div";
-      }
-      if (div.className === "project-div" && i > clickedDivID) {
-        div.className = "project-div-shifted";
-      }
-    });
-  };
-
-  //shifts divs back up 150px when the collapse button is clicked
-  const shiftDivsUp = () => {
-    const projDivs = document.querySelectorAll("[data-id]");
-    const projDivsArray = Array.from(projDivs);
-    projDivsArray.forEach(function (div) {
-      if (div.className === "project-div-shifted") {
-        div.className = "project-div";
+        calendarValue = '';
+        newProjectInput.value = '';
       }
     });
   };
 
   //creates a form where a new task is entered on the project div
   const createNewTaskForm = (project, projectDiv) => {
-    const overlayDiv = document.createElement("div");
-    overlayDiv.id = "overlay";
+    const overlayDiv = document.createElement('div');
+    overlayDiv.id = 'overlay';
     container.appendChild(overlayDiv);
 
-    const newTaskForm = document.createElement("div");
-    newTaskForm.className = "new-task-form";
-    newTaskForm.id = "task-form-in-div";
+    const newTaskForm = document.createElement('div');
+    newTaskForm.className = 'new-task-form';
+    newTaskForm.id = 'task-form-in-div';
 
     const newTaskNameInput = createDomInput(
-      "newTaskNameInput",
-      "form-group input-group form-control",
-      "Task Name"
+      'newTaskNameInput',
+      'form-group input-group form-control',
+      'Task Name'
     );
-    newTaskNameInput.id = "task-name-input";
+    newTaskNameInput.id = 'task-name-input';
     newTaskForm.appendChild(newTaskNameInput);
 
     const newTaskDescriptionInput = createDomInput(
-      "newTaskDescriptionInput",
-      "form-group input-group form-control",
-      "Describe this task"
+      'newTaskDescriptionInput',
+      'form-group input-group form-control',
+      'Describe this task'
     );
-    newTaskDescriptionInput.id = "describe";
+    newTaskDescriptionInput.id = 'describe';
     newTaskForm.appendChild(newTaskDescriptionInput);
 
-    const newTaskPriority = ["Low", "Medium", "High"];
+    const newTaskPriority = ['Low', 'Medium', 'High'];
     newTaskPriority.forEach((priority) => {
-      const radios = document.createElement("div");
+      const radios = document.createElement('div');
       radios.className = priority;
       radios.innerHTML = `<input type = "radio" name='priority' class = 'radio'
             
@@ -226,52 +170,55 @@ const renderInterface = (() => {
       newTaskForm.appendChild(radios);
     });
 
-    const collapseTaskButton = document.createElement("button");
-    collapseTaskButton.className = "collapse-task-form";
-    newTaskForm.appendChild(collapseTaskButton);
-    collapseTaskButton.addEventListener("click", () => {
-      overlayDiv.style.display = "none";
+    const taskFormButtonDiv = document.createElement('div');
+    taskFormButtonDiv.className = 'task-button-div';
+    const collapseTaskButton = document.createElement('button');
+    collapseTaskButton.innerHTML = 'Close';
+    collapseTaskButton.className = 'collapse-task-form';
+    taskFormButtonDiv.appendChild(collapseTaskButton);
+    collapseTaskButton.addEventListener('click', () => {
+      overlayDiv.style.display = 'none';
     });
 
-    const newTaskButton = document.createElement("button");
-    newTaskButton.innerHTML = "Add task to " + project.getTitle();
-    newTaskButton.className = "new-task-button";
-    newTaskForm.appendChild(newTaskButton);
+    const newTaskButton = document.createElement('button');
+    newTaskButton.innerHTML = 'Add task to ' + project.getTitle();
+    newTaskButton.className = 'new-task-button';
+    taskFormButtonDiv.appendChild(newTaskButton);
+    newTaskForm.appendChild(taskFormButtonDiv);
 
     overlayDiv.appendChild(newTaskForm);
 
-    newTaskButton.addEventListener("click", () => {
+    newTaskButton.addEventListener('click', () => {
       event.preventDefault();
       const priorityBtns = document.querySelector(
         'input[name="priority"]:checked'
       ).id;
       if (!priorityBtns) {
-        alert("Please Select Priority");
+        alert('Please Select Priority');
       }
       if (!newTaskDescriptionInput.value || !newTaskNameInput.value) {
-        alert("Please Fill In Both Fields!");
+        alert('Please Fill In Both Fields!');
       } else {
         let newListItem = ListItem(
           newTaskNameInput.value,
           newTaskDescriptionInput.value,
           priorityBtns
         );
-        console.log(newListItem);
         project.setTodos(newListItem);
         createListItem(projectDiv, project);
-        newTaskForm.style.display = "none";
-        overlayDiv.style.display = "none";
+        newTaskForm.style.display = 'none';
+        overlayDiv.style.display = 'none';
       }
     });
   };
 
   //creates an input elemnt for all forms on the page
   const createDomInput = (elementName, classNames, inputPlaceholder) => {
-    elementName = document.createElement("input");
+    elementName = document.createElement('input');
     elementName.className = classNames;
-    elementName.type = "text";
+    elementName.type = 'text';
     elementName.placeholder = inputPlaceholder;
-    elementName.value = "";
+    elementName.value = '';
     return elementName;
   };
 
@@ -282,42 +229,43 @@ const renderInterface = (() => {
     console.log(index);
     let taskItems = project.toDos;
     let taskList = document.querySelector(`[data-number='${index}']`);
-    console.log(taskList);
+    taskList.className = 'task-list-div';
 
     taskItems.forEach(function (taskItem, index, array) {
       if (index === array.length - 1) {
-        const taskItemToAppend = document.createElement("li");
-        taskItemToAppend.className = "list-items";
-        taskItemToAppend.innerHTML = `<div class = 'task-list-item'>${taskItem.title}</div> 
-                <div class='description-list-item'>${taskItem.description}</div> 
+        const taskItemToAppend = document.createElement('div');
+        taskItemToAppend.className = 'list-items';
+        taskItemToAppend.innerHTML = `<p class = 'task-list-item'>${taskItem.title}</p> 
+                <p class='description-list-item'>${taskItem.description}</p> 
                 <div class='priority-list-item'></div>`;
         taskList.appendChild(taskItemToAppend);
-        const priorityDot = document.querySelector(".priority-list-item");
+        const priorityDot = document.querySelector('.priority-list-item');
         console.log(priorityDot);
 
-        if (taskItem.priority === "High") {
-          priorityDot.className = "high-priority";
-        } else if (taskItem.priority === "Medium") {
-          priorityDot.className = "medium-priority";
-        } else if (taskItem.priority === "Low") {
-          priorityDot.className = "low-priority";
+        if (taskItem.priority === 'High') {
+          priorityDot.className = 'dot high-priority';
+        } else if (taskItem.priority === 'Medium') {
+          priorityDot.className = 'dot medium-priority';
+        } else if (taskItem.priority === 'Low') {
+          priorityDot.className = 'dot low-priority';
         }
 
-        const completedButton = document.createElement("button");
-        completedButton.className = "completed-button";
+        const completedButton = document.createElement('button');
+        completedButton.className = 'img-button completed-button';
 
         taskItemToAppend.appendChild(completedButton);
-        projectDiv.className === "project-div-expanded"
-          ? (taskList.style.display = "grid")
-          : (taskList.style.display = "none");
+        projectDiv.className === 'project-div-expanded'
+          ? (taskList.style.display = 'grid')
+          : (taskList.style.display = 'none');
 
-        completedButton.addEventListener("click", () => {
+        completedButton.addEventListener('click', () => {
           taskItem.completedTask === true
             ? (taskItem.completedTask = false)
             : (taskItem.completedTask = true);
-          completedButton.className === "completed-button"
-            ? (completedButton.className = "completed-button-complete")
-            : (completedButton.className = "completed-button");
+          completedButton.className === 'img-button completed-button'
+            ? (completedButton.className =
+                'img-button completed-button-complete')
+            : (completedButton.className = 'img-button completed-button');
         });
       }
     });
